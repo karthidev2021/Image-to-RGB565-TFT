534aebe79cd03dc762579cac0f6b4eb415d45612
karthi <karthi2002smk@gmail.com>
2023-06-03 14:00:56 +0530

    fixed bug

--- test.py
+++ test.py
@@ -7,11 +7,12 @@ import os
 
 class TFT_IMG_RAWFILE_MAKER:
     
-    def __init__(self,outputFileName,outputFolderPath,imgFolderPath):
+    def __init__(self,outputFileName,outputFolderPath,imgFolderPath,variable):
         self.list_of_Images=[]
         self.outputFileName=outputFileName
         self.outFolderPath=outputFolderPath
         self.imgFolderPath=imgFolderPath
+        self.variable=variable
 
     def writeToFile(self,data,mode='a'):
         path=self.outFolderPath+"/"+self.outputFileName+".h"
@@ -20,8 +21,13 @@ class TFT_IMG_RAWFILE_MAKER:
         
     def initialWrite(self):
         self.list_of_Images=os.listdir(self.imgFolderPath)
-        code="int frames="+str(len(self.list_of_Images))+";\n"
-        code+="const unsigned short load[][16384] PROGMEM={\n"
+        image=Image.open(self.imgFolderPath+"/"+self.list_of_Images[0])
+        totalPixel=image.size[0]*image.size[1]
+        image.close()
+        code="int {}_frames={};\n".format(self.variable,len(self.list_of_Images))
+        code+="int {}_width={};\n".format(self.variable,image.size[0])
+        code+="int {}_height={}\n;".format(self.variable,image.size[1])
+        code+="const unsigned short {}[][{}] PROGMEM={{\n".format(self.variable,totalPixel)
         self.writeToFile(code)
 
     def finalWrite(self):
@@ -30,6 +36,7 @@ class TFT_IMG_RAWFILE_MAKER:
 
 
     def imageToHexSeq(self):
+        count=0
         for imgName in self.list_of_Images:
             code="{\n"
             self.writeToFile(code)
@@ -44,10 +51,12 @@ class TFT_IMG_RAWFILE_MAKER:
                     r,g,b=image.getpixel((x,y))
                     imgInHex=hex(self.rgb_to_rgb565(r,g,b))
                     # imgInHex="0x{:2X}{:2X}{:2X}".format(r,g,b)
-                    if newLine: 
+                    if newLine or count==16: 
                         imgInHex="\n"+imgInHex
                         newLine=False
+                        if(count==16): count=0
                     frame.append(imgInHex)
+                    count+=1
 
                 newLine=True
             code=",".join(frame)
@@ -83,20 +92,26 @@ class App(tk.Tk):
 
         try:
 
-            OutFileName=input("Output FileName : ")
-            print("File path : ",end="")
-            OutFolderPath=filedialog.askdirectory()
-            if OutFolderPath=="": raise Exception
-            print(OutFolderPath)
-
-            imgFolderPath=filedialog.askdirectory()
+            print("Image File path : ",end="",flush=True)
+            imgFolderPath=filedialog.askdirectory(title="Select image folder")
             if imgFolderPath=="": raise Exception
-            print(imgFolderPath + " is Selected")
+            print(imgFolderPath)
 
-            converter=TFT_IMG_RAWFILE_MAKER(OutFileName,OutFolderPath,imgFolderPath)
-            if(input("start process?? (yes/no)")=="yes"): converter.start()
+            OutFileName=input("Output FileName : ")
+            if OutFileName=="": OutFileName="output"
+
+            print("Outfile File path : ",end="",flush=True)
+            outFolderPath=filedialog.askdirectory(title="Select output folder")
+            if outFolderPath=="": raise Exception
+            print(outFolderPath)
+
+            variableName=input("Output Variable Name : ")
+            if variableName=="": variableName="image_var"
+
+            converter=TFT_IMG_RAWFILE_MAKER(OutFileName,outFolderPath,imgFolderPath,variableName)
+            if(input("start process?? (yes/no) : ")=="yes"): converter.start()
             
-        except:print("Error occured!!")
+        except Exception as e:print(e)
 
         self.quit()
         print("See you next time ;)")
